108d648a10680b3469683773361fc1e97a5e2376
Xhevdet <xhevdet1996@gmail.com>
2020-03-06 15:38:20 +0100

    auth form added

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 
 import { AuthService } from './auth.service';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -11,6 +12,7 @@ import { LoadingController } from '@ionic/angular';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(
     private authService: AuthService, 
@@ -34,5 +36,25 @@ export class AuthPage implements OnInit {
         }, 1500)
       });
     
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+    
+
+    if(this.isLogin) {
+      // Send a request to login servers 
+    } else {
+      // Send a request to signup servers
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 }
